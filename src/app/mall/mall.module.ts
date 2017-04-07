b4dd61fd19a35e20115e1301dc6d0950c9206f25
vincent-lee90 <vincent_lee90@sina.cn>
2017-04-07 18:05:23 +0800

    commit

--- src/app/mall/mall.module.ts
+++ src/app/mall/mall.module.ts
@@ -3,12 +3,14 @@ import {RouterModule} from '@angular/router';
 import {mallRoutes} from './mall.routes';
 import {StoresComponent} from "./stores/stores.component";
 import {MallComponent} from "./mall/mall.component";
+import{MainMenuModule} from "../share/main-menu/main-menu.module"
 @NgModule({
   declarations: [
     StoresComponent,
     MallComponent
   ],
   imports: [
+    MainMenuModule,
     RouterModule.forChild(mallRoutes)
   ],
   providers: []
