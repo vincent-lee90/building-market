3b34e4d76ce3a6b67212733d8a9d5d5c6e17fb7d
lee <vincent_lee90@sina.cn>
2017-04-13 08:42:06 +0800

    commit

--- src/app/mall/mall.routes.ts
+++ src/app/mall/mall.routes.ts
@@ -1,11 +1,14 @@
 import {StoresComponent} from "./stores/stores.component";
 import {MallComponent} from "./mall/mall.component";
-export const mallRoutes=[
+export const mallRoutes = [
   {
-    path:'',
-    component:MallComponent
-  },{
-  path:'stores/:category',
-    component:StoresComponent
+    path: '',
+    component: MallComponent
+  }, {
+    path: 'stores/:category',
+    component: StoresComponent
+  }, {
+    path: 'stores',
+    component: StoresComponent
   }
 ];
--- src/app/mall/stores/stores.component.ts
+++ src/app/mall/stores/stores.component.ts
@@ -1,9 +1,13 @@
-import {Component} from "@angular/core";
+import {Component,OnInit} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
 @Component({
   selector:"stores",
   templateUrl:"./stores.component.html",
   styleUrls:["./stores.component.less"]
 })
-export class StoresComponent{
-
+export class StoresComponent implements OnInit{
+  constructor(private route:ActivatedRoute){}
+  ngOnInit(){
+    console.log(this.route.params);
+  }
 }
