650f3f938ca4b9bd79ce5cf7dafcd7bed353e938
vincent-lee90 <vincent_lee90@sina.cn>
2017-06-06 17:55:33 +0800

    commit

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router'
 import {User} from '../model/user-model';
 import {AccountService} from '../services/account.service';
 import {AuthGuardService} from '../../auth-guard/auth-guard.service';
+import {AppDialogService} from "../../share/myDialog/app-alert/app-dialog.service";
 @Component({
   selector:'login',
   templateUrl:'./login.component.html',
@@ -11,7 +12,7 @@ import {AuthGuardService} from '../../auth-guard/auth-guard.service';
 })
 export class LoginComponent implements OnInit{
   public user:User=new User();
-  constructor(private accountService:AccountService,private authService:AuthGuardService,private router:Router){
+  constructor(private accountService:AccountService,private authService:AuthGuardService,private router:Router,private appDialogService:AppDialogService){
 
   }
   login(){
@@ -20,6 +21,8 @@ export class LoginComponent implements OnInit{
         if(data.statusCode==200){
           this.authService.isLogin=true;
           this.router.navigate([this.authService.redirectUrl]);
+        }else{
+          this.appDialogService.setAlert(data.message)
         }
       });
   }
