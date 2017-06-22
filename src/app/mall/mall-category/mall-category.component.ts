7c59300b69167aa711da9735ea86189eb4df6981
vincent-lee90 <vincent_lee90@sina.cn>
2017-06-22 10:37:16 +0800

    commit

--- src/app/mall/mall-category/mall-category.component.ts
+++ src/app/mall/mall-category/mall-category.component.ts
@@ -1,12 +1,20 @@
-import {Component,OnInit} from '@angular/core';
+import {Component,OnInit,Output,EventEmitter} from '@angular/core';
 @Component({
   selector:'mall-category',
   templateUrl:'./mall-category.component.html',
   styleUrls:['./mall-category.component.less']
 })
 export class MallCategoryComponent implements OnInit{
+  isShowCategories=false;
+  @Output() onSelect=new EventEmitter<string>();
   constructor(){
 
+  }
+  select(code:string){
+    this.onSelect.emit(code);
+  }
+  showCategories(){
+    this.isShowCategories=!this.isShowCategories;
   }
   ngOnInit(){
 
