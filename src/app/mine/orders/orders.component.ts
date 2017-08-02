f0a2ce98dc0f3d921de8876418d441d7a0645f50
vincent-lee90 <vincent_lee90@sina.cn>
2017-08-02 16:55:11 +0800

    commit

--- src/app/mine/orders/orders.component.ts
+++ src/app/mine/orders/orders.component.ts
@@ -1,31 +1,29 @@
-import {Component,OnInit} from '@angular/core';
-import {ActivatedRoute,Params} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 import {MineService} from "../service/mine.service";
 import {Order} from "../../mall/model/order.model";
 
 @Component({
-  selector:'orders',
-  templateUrl:'./orders.component.html',
-  styleUrls:['./orders.component.less']
+  selector: 'orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.less']
 })
-export class OrdersComponent implements OnInit{
-  private orders:Order[]=[];
-  private current_tab='';
-  constructor(private route:ActivatedRoute,private mineService:MineService){}
-  getOrdersByStatus(){
-    this.route.params.switchMap((params:Params)=>{
-      this.current_tab=params['orderStatus'];
-      console.log(1);
-       return this.mineService.getOrdersByStatus(this.current_tab)
-    }).subscribe(data=>{
-      this.orders=data;
+export class OrdersComponent implements OnInit {
+  private orders: Order[] = [];
+  private current_tab = '';
+
+  constructor(private route: ActivatedRoute, private mineService: MineService) {
+  }
+
+  getOrdersByStatus() {
+    this.route.params.subscribe((params: Params) => {
+       this.mineService.getOrdersByStatus(params['orderStatus']).subscribe(data=>{
+         this.orders=data;
+       })
     })
   }
-  changeTab(tab){
-
-  }
-  ngOnInit(){
+  ngOnInit() {
     this.getOrdersByStatus()
   }
 }
