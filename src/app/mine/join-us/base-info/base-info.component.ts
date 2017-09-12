84887977e43c1538a405af79fdeb14670f12b848
vincent-lee90 <vincent_lee90@sina.cn>
2017-09-12 18:01:50 +0800

    commit

--- src/app/mine/join-us/base-info/base-info.component.ts
+++ src/app/mine/join-us/base-info/base-info.component.ts
@@ -1,30 +1,34 @@
-import {Component,OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AppDialogService} from "../../../share/myDialog/app-alert/app-dialog.service";
 import {MineService} from "../../service/mine.service";
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-  selector:'base-info',
-  templateUrl:'base-info.component.html',
-  styleUrls:['./base-info.component.less']
+  selector: 'base-info',
+  templateUrl: 'base-info.component.html',
+  styleUrls: ['./base-info.component.less']
 })
-export class BaseInfoComponent implements OnInit{
-  public baseInfo={};
-  constructor(private appDialogService:AppDialogService,private mineService:MineService,private router:Router,private route:ActivatedRoute){}
-  toStoreInfo(f:NgForm){
-    if(f.form.controls['name'].invalid){
+export class BaseInfoComponent implements OnInit {
+  public baseInfo = {};
+
+  constructor(private appDialogService: AppDialogService, private mineService: MineService, private router: Router, private route: ActivatedRoute) {
+  }
+
+  toStoreInfo(f: NgForm) {
+    if (f.form.controls['name'].invalid) {
       this.appDialogService.setAlert('请填入姓名');
       return
     }
-    if(f.form.controls['telephone'].invalid){
+    if (f.form.controls['telephone'].invalid) {
       this.appDialogService.setAlert('请填入手机号');
       return
     }
-    this.mineService.setBaseInfo(f.value.name,f.value.telephone);
-    this.router.navigate(["../store-info"],{relativeTo:this.route});
+    this.mineService.setBaseInfo(f.value.name, f.value.telephone);
+    this.router.navigate(["../store-info"], {relativeTo: this.route});
   }
-  ngOnInit(){
+
+  ngOnInit() {
 
   }
 }
