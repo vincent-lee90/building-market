8d67f777ea6e21f653317c5def139c3eb4e0a826
vincent-lee90 <vincent_lee90@sina.cn>
2017-10-20 15:18:34 +0800

    commit

--- src/app/mine/my-store/edit-product/edit-product.component.ts
+++ src/app/mine/my-store/edit-product/edit-product.component.ts
@@ -1,13 +1,15 @@
 import {Component, OnInit} from "@angular/core";
 import {MineService} from "../../service/mine.service";
+import {Product} from "../../../mall/model/product.model";
 @Component({
   selector:'edit-product',
   templateUrl:"./edit-product.component.html",
   styleUrls:['./edit-product.component.less']
 })
 export class EditProductComponent implements OnInit{
-  constructor(private mineService:MineService){}
   public myStoreInfo={};
+  public product:Product;
+  constructor(private mineService:MineService){}
   getProduct(){
 
   }
