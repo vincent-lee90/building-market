cdd1ab963950e660bde20c3947c908c8d059cdd4
vincent-lee90 <vincent_lee90@sina.cn>
2017-11-01 17:12:00 +0800

    commit

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -3,6 +3,7 @@ import {User} from '../model/user-model';
 import {AccountService} from '../services/account.service';
 import {AuthGuardService} from '../../auth-guard/auth-guard.service';
 import {Router} from '@angular/router';
+import {AppDialogService} from "../../share/myDialog/app-alert/app-dialog.service";
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
@@ -12,19 +13,21 @@ import {Router} from '@angular/router';
 export class RegisterComponent {
   public user = new User();
 
-  constructor(private accountService: AccountService,private authService:AuthGuardService,private router:Router) {
+  constructor(private accountService: AccountService, private authService: AuthGuardService, private router: Router, private appDialogService: AppDialogService) {
   }
-
   register(rePassword) {
-    if(this.user.password!==rePassword){
+    if (this.user.password !== rePassword) {
       return;
     }
     this.accountService.register(this.user)
       .subscribe(data => {
-        if(data.statusCode==200){
-          this.authService.isLogin=true;
-          this.router.navigate([this.authService.redirectUrl]);
+        if (data.statusCode == 200) {
+          this.appDialogService.setAlert("注册成功");
+          this.authService.isLogin = true;
+        } else {
+          this.appDialogService.setAlert("注册失败了");
         }
+        this.router.navigate([this.authService.redirectUrl]);
       });
   }
 }
