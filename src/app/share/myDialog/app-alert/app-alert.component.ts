fc6a132d8b1cba4a0c933e16e100bc9b32d99001
vincent-lee90 <vincent_lee90@sina.cn>
2018-02-28 10:21:27 +0800

    add animations for alert

--- src/app/share/myDialog/app-alert/app-alert.component.ts
+++ src/app/share/myDialog/app-alert/app-alert.component.ts
@@ -1,9 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 import {AppDialogService} from '../service/app-dialog.service';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 @Component({
   selector: 'app-alert',
   templateUrl: 'app-alert.component.html',
-  styleUrls: ['app-alert.component.less']
+  styleUrls: ['app-alert.component.less'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({})),
+      transition('void=>*', [
+        animate(200, keyframes([
+          style({opacity: 0, transform: 'scale(.7)', offset: 0}),
+          style({opacity: 1, transform: 'scale(1)',     offset: 1.0})
+        ]))
+      ]),
+      transition('*=>void', [
+        animate(200, keyframes([
+          style({opacity: 1, transform: 'scale(1)',offset: 0}),
+          style({opacity: 0, transform: 'scale(.7)',offset: 1.0})
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppAlertComponent implements OnInit {
   constructor(private appDialogService: AppDialogService) {
